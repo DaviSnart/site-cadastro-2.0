b097ff4e32d5b7fee9d607b25a36b7b59a6a6484
DaviSnart <davi.soares9876543210@gmail.com>
2024-09-19 11:05:04 -0300

    alteração na BASEURL

--- src/pages/services/api.js
+++ src/pages/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/'
+    baseURL: 'https://api-registro-de-usu-rios-2.vercel.app/'
 })
 
 export default api
